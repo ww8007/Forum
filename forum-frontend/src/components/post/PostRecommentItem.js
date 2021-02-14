819075ebbb1ae696dee5f68c8cc2194449047cef
ww8007 <wshmin@naver.com>
2021-02-14 20:45:45 +0900

    While Insert recomment

--- forum-frontend/src/components/post/PostRecommentItem.js
+++ forum-frontend/src/components/post/PostRecommentItem.js
@@ -1,7 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const PostRecommentItem = ({ recomment, onRecomment }) => {
-  return <div></div>;
+  const { id } = recomment;
+  const [text, setText] = useState('');
+  const [set, onSet] = useState(false);
+  const onSubmit = (e) => {
+    e.preventDefault();
+    onRecomment(id, text);
+    setText('');
+  };
+  return <div>{recomment.text}</div>;
 };
 
 export default PostRecommentItem;
